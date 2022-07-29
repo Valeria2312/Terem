92e92dcc344a42c6c3775e89dbef6c1a6875b9d1
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-07-29 17:40:08 +0300

    add code to form

--- js/form.js
+++ js/form.js
@@ -1,16 +1,21 @@
 const submit = document.querySelector(".btn-success");
 const responseForm = document.querySelector(".response_form");
 submit.addEventListener("click", newForm);
+// let data1 =document.querySelectorAll('.request-elem');
+// console.log(data1);
 
 
 async function newForm() {
-    let data = Array.from(document.querySelectorAll('.request-elem'), v=> v.value);
+
+    let res = [];
+    Array.from(document.querySelectorAll('.form')).forEach(e => {
+        res.push((Array.from(e.querySelectorAll('input,select')).map(e => e.value)));
+    });
+    console.log(res);
 
     await fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
-        body: JSON.stringify({
-            data: data,
-        }),
+        body: JSON.stringify(res),
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
@@ -23,5 +28,6 @@ async function newForm() {
         })
         .then(json => {
             responseForm.innerHTML = 'response: ' + JSON.stringify(json);
+            console.log(JSON.stringify(json))
         })
 }
